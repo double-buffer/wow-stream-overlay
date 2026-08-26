f7305e9871a542f97157d02a43c8ad3db570a1fc
Thomas Decroyère <thomas.decroyere@outlook.com>
2026-08-26 11:09:56 +0200

    Expose prerelease application version

--- src/WoWStreamOverlay/ApplicationInfo.cs
+++ src/WoWStreamOverlay/ApplicationInfo.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace WowStreamOverlay;
 
 public static class ApplicationInfo
@@ -5,5 +7,21 @@ public static class ApplicationInfo
     public const string Name = "WoW Stream Overlay";
     public const string RepositoryUrl = "https://github.com/double-buffer/wow-stream-overlay";
 
-    public static string Version => typeof(ApplicationInfo).Assembly.GetName().Version?.ToString(3) ?? "unknown";
+    public static string Version
+    {
+        get
+        {
+            var informationalVersion = typeof(ApplicationInfo).Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+
+            if (!string.IsNullOrWhiteSpace(informationalVersion))
+            {
+                var metadataSeparator = informationalVersion.IndexOf('+');
+                return metadataSeparator < 0 ? informationalVersion : informationalVersion[..metadataSeparator];
+            }
+
+            return typeof(ApplicationInfo).Assembly.GetName().Version?.ToString(3) ?? "unknown";
+        }
+    }
 }
